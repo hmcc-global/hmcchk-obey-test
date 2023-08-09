3e776d34bbdac46c3d209e606ca31ed2a3815fc1
Kristian Suhartono <blackxephos2@gmail.com>
2023-08-10 00:01:42 +0800

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hmcchk-obey-test/",
   plugins: [react()],
   esbuild: {
     loader: "jsx",
